be27d75a8e8ce3bd3aa9b337df28c4f32d91b857
sajani <deepasaj@thoughtworks.com>
2013-01-21 10:28:16 +0530

    my first welcome screen

---
+++ sample/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample::Application.config.secret_token = 'e9ea101adfbc8387a6aa6fe6f6c06610b085ea6f2a49a515e2fbd4344f333195ad3c2287982dc430d6799294e887b34311140a1d69a527ffa102fcd8829e8056'
